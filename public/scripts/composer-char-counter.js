c9537a7d26689d0095ec9d05af8becb3dada01ab
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-02-07 04:43:04 +0000

    fixed counter to turn red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-  $("#tweet-text").on('keyup', function() {
-  let tweetCount = $(this).val().length;
-  $("output").text(140-tweetCount);
-  let $tc = $(this).closest('.input-box').siblings('.bottomline').find('.counter').text(140 - tweetCount);
-if (tweetCount <= 140) {
-  $tc.removeClass('error');
-} else {
-  $tc.addClass('error');
+const countNumber = 140;
+function updateCounter() {
+  const counter = $("#tweet-text").val().length;
+  $('.counter').text(countNumber - counter);
+  if ($('.counter').text() < 0) {
+    $('.counter').addClass('error');
+  } else {
+    return $('.counter').removeClass('error');
+  }
 }
-});
+$(document).ready(function () {
+  $("#tweet-text").on('input', updateCounter);
 }); 
